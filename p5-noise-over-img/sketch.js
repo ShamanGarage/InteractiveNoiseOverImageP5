61be22f530a6447fb61a1d237953c271953924ec
pabgre <pablosan@tutanota.com>
2021-04-18 20:40:08 +0200

    mobile support

--- p5-noise-over-img/sketch.js
+++ p5-noise-over-img/sketch.js
@@ -2,17 +2,19 @@ let img;
 
 function preload(){
     img = loadImage("aritz-min_opt_white.jpg");
+    imgNoise = loadImage("aritz-min_opt_white.jpg")
 }
 
 function setup() {
   createCanvas(400, 579);
 
   img.resize(400, 579);
+  imgNoise.resize(400, 579);
   image(img, 0, 0, img.width, img.height);
   img.loadPixels();
 }
 document.getElementsByTagName("BODY")[0].onmousemove = function (){
-  loadPixels();
+  imgNoise.loadPixels();
 
 
   for (var i = 0; i < img.width; i++) {
@@ -22,22 +24,24 @@ document.getElementsByTagName("BODY")[0].onmousemove = function (){
         let g =  img.pixels[index + 1];
         let b = img.pixels[index + 2];
 
-        let slider = Math.max(0, Math.min(mouseX +5, img.width))
+        let slider = Math.max(0, Math.min(mouseX + 5, img.width))
         let brightness = (((r + g + b)/3)/255.0) * img.width
 
         if (brightness <= slider){
-          pixels[index + 0] = 0;
-          pixels[index + 1] = 0;
-          pixels[index + 2] = 0;
-          pixels[index + 3] = random(255);
+          imgNoise.pixels[index + 0] = 0;
+          imgNoise.pixels[index + 1] = 0;
+          imgNoise.pixels[index + 2] = 0;
+          imgNoise.pixels[index + 3] = random(255);
         }else{
-          pixels[index + 0] = r;
-          pixels[index + 1] = g;
-          pixels[index + 2] = b;
-          pixels[index + 3] = 255;
+          imgNoise.pixels[index + 0] = r;
+          imgNoise.pixels[index + 1] = g;
+          imgNoise.pixels[index + 2] = b;
+          imgNoise.pixels[index + 3] = 255;
         }
 
     }
   }
-  updatePixels();
+  imgNoise.updatePixels();
+  clear();
+  image(imgNoise, 0, 0, img.width, img.height);
 };
